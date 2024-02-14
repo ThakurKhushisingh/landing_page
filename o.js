73ddba9d01b7e14c96a14263f3a5d61a1b55227e
Khushi Singh <122673305+ThakurKhushisingh@users.noreply.github.com>
2024-02-14 17:57:29 +0530

    Add files via upload
    using javascript html and css.......

---
+++ o.js
@@ -0,0 +1,13 @@
+ function b(){
+
+      document.getElementById(ul);
+        ul.style.background='grey';
+         
+    }
+    function c(){
+        document.getElementById(ul);
+        ul.style.background='black';   
+    }
+    function r(){
+        alert('Currently Unavailable');
+    }
